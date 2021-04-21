dec50ecec9cae2f8bb8846626a06269bddd10932
shin1616 <s.murata0416@gmail.com>
2021-04-21 13:38:12 +0900

    ビューの改善

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -32,8 +32,26 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+    @book = Book.destroy(params[:id])
+    if @book.destroy
+      redirect_to root_path 
+    end
+  end
+
 
   private
+
+  def tweet_params
+    params.require(:tweet).permit(:name, :image, :text).merge(user_id: current_user.id)
+  end
+
   def book_params
   	params.require(:book).permit(:title, :author, :mediumImageUrl, :itemUrl, :publisherName, :itemPrice, :size, :isbn).merge(user_id: current_user.id)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'books#index'
   resources :books
+  resources :users, only: :show
 end
